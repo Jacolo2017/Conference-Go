819356d58d327d85ff9cb1f5ce3e5bb573c7fab8
Jack <jacolo2017@gmail.com>
2022-05-31 16:46:01 -0600

    Push May 31st

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
-from events.models import Conference
-from .models import Attendee
+from .models import Attendee, ConferenceVO
 from django.views.decorators.http import require_http_methods
 from common.json import ModelEncoder
 import json
 
+
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
 
 class AttendeeDetailEncoder(ModelEncoder):
     model = Attendee
@@ -26,7 +29,7 @@ class AttendeeListEncoder(ModelEncoder):
     properties = ["name"]
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -47,7 +50,7 @@ def api_list_attendees(request, conference_id):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.all()
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendee": attendees},
             encoder=AttendeeListEncoder,
@@ -55,18 +58,19 @@ def api_list_attendees(request, conference_id):
     else:
         content = json.loads(request.body)
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = content["conference"]
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
-                {"message": "Invalid conference id"}
+                {"message": "Invalid conference id"},
                 status=400
             )
         attendee = Attendee.objects.create(**content)
         return JsonResponse(
             attendee,
-            encoder=AttendeeDetailEncoder
-            safe=False
+            encoder=AttendeeDetailEncoder,
+            safe=False,
         )
 
 
